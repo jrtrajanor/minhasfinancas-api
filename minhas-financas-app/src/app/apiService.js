b6a64352960fc399d5a731e08a77e34d57466c89
Júnior Trajano Rodrigues <junior.rodrigues@softplan.com.br>
2020-05-02 21:15:08 -0300

    commit

--- minhas-financas-app/src/app/apiService.js
+++ minhas-financas-app/src/app/apiService.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://jrfinancas-api.herokuapp.com/', 
+    //'http://localhost:8080',
     headers:{'x-apikey': '59a7ad19f5a9fa0808f11931',
              'Access-Control-Allow-Origin' : '*',
              'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
